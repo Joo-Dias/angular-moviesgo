5c4c03dc95315d52677e4219b18846fa2b47282e
João Dias <joaovitorelf@gmail.com>
2023-09-21 12:26:00 -0300

    menuBar-myLinks

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import {
+  faHome,
+  faArrowUpWideShort,
+  faCompass,
+  faShuffle
+} from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.scss']
 })
 export class MenuBarComponent {
+  Home = faHome;
+  Trending = faArrowUpWideShort;
+  Compass = faCompass;
+  Shuffle = faShuffle;
+
+  Linkedin = faLinkedin;
+  Github = faGithub;
 
 }
